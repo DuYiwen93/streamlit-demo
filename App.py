a382254e9cd517490493d86df664381615c996f5
DuYiwen93 <163510801+DuYiwen93@users.noreply.github.com>
2024-04-30 13:02:26 +0800

    Update App.py

--- App.py
+++ App.py
@@ -134,34 +134,11 @@ with st.sidebar:
     st.markdown('Wang Shuhui',
                 unsafe_allow_html=True)
     st.caption("Wang Shuhui (1912-1985), courtesy name Yufen, born in Tianjin, was a famous modern female painter of heavy color figures. She is good at inheriting the excellent tradition of line drawing in Chinese painting and absorbing the perspective and anatomical method of Western painting.")
-
-
-
-    # ---------------------Example art images------------------------------
-    #这里展开侧边栏的两列设计，example里放的是图，如果不需要两列的设计就把这一大段全部注释掉，在上面单栏的地方加内容
     col1, col2 = st.columns(2)
     with col1:
-        st.image(image="content1.jpg")
+        st.image(image="wsh1.png")
     with col2:
-        st.image(image="art1.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="content2.jpg")
-    with col2:
-        st.image(image="art2.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="content3.jpg")
-    with col2:
-        st.image(image="art3.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="content4.jpg")
-    with col2:
-        st.image(image="art4.png")
+        st.image(image="wsh2.jpg")
 
 
 
@@ -170,78 +147,3 @@ with st.sidebar:
 
 
 
-#这一团是作者（这篇网页设计的作者）的个人信息，如果不要全部注释掉，要的话适当改其中内容
- # ----------------------------------------------------------------------
-
-    # line break
-    st.markdown(" ")
-    # About the programmer
-    st.markdown("## Made by **Deepesh Mhatre** \U0001F609")
-    st.markdown("Contribute to this project at "
-                "[*github.com/deepeshdm*](https://github.com/deepeshdm/Realtime_Face_Detection)")
-
-
-
-
-
-#这团别管，嫌烦全部删掉
-# # -------------Body Section------------------------------------------------
-
-# # Upload Images
-# col1, col2 = st.columns(2)
-# content_image = None
-# style_image = None
-# with col1:
-#     content_image = st.file_uploader(
-#         "Upload Content Image (PNG & JPG images only)", type=['png', 'jpg'])
-# with col2:
-#     style_image = st.file_uploader(
-#         "Upload Style Image (PNG & JPG images only)", type=['png', 'jpg'])
-
-
-# st.markdown("</br>", unsafe_allow_html=True)
-# st.warning('NOTE : You need atleast Intel i3 with 8GB memory for proper functioning of this application. ' +
-#    ' Images greater then (2000x2000) are resized to (1000x1000).')
-
-
-# if content_image is not None and style_image is not None:
-
-#     with st.spinner("Styling Images...will take about 20-30 secs"):
-
-#         content_image = Image.open(content_image)
-#         style_image = Image.open(style_image)
-
-#         # Convert PIL Image to numpy array
-#         content_image = np.array(content_image)
-#         style_image = np.array(style_image)
-
-#         # Path of the pre-trained TF model
-#         model_path = r"model"
-
-#         # output image
-#         styled_image = transfer_style(content_image, style_image, model_path)
-#         if style_image is not None:
-#             # some baloons
-#             st.balloons()
-
-#         col1, col2 = st.columns(2)
-#         with col1:
-#             # Display the output
-#             st.image(styled_image)
-#         with col2:
-
-#             st.markdown("</br>", unsafe_allow_html=True)
-#             st.markdown(
-#                 "<b> Your Image is Ready ! Click below to download it. </b>", unsafe_allow_html=True)
-
-#             # de-normalize the image
-#             styled_image = (styled_image * 255).astype(np.uint8)
-#             # convert to pillow image
-#             img = Image.fromarray(styled_image)
-#             buffered = BytesIO()
-#             img.save(buffered, format="JPEG")
-#             st.download_button(
-#                 label="Download image",
-#                 data=buffered.getvalue(),
-#                 file_name="output.png",
-#                 mime="image/png")
